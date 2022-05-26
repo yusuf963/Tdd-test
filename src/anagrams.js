1f0ac5260175780fbde39866678add430488c4b4
M.Yusuf Musa <mohammad963yusuf@gmail.com>
2022-05-26 23:51:23 +0100

    add

--- src/anagrams.js
+++ src/anagrams.js
@@ -1,6 +1,7 @@
 export const isAnagram =(string1,string2)=>{
     const string1letters = string1.split('')
     const string2letters = string2.split('')
+    const string3letters = string3.split('')
 
     return string1letters.every(letter=>
         string2letters.includes(letter))
